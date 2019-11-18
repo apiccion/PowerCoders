934b36ac54805a383b32f22b56e2d3167f38ccfc
RoboYorkie [Ali Piccioni] <roboyorkie@gmail.com>
2019-11-18 21:22:06 +0100

    Remove failing tests so that students may discover them.

--- calc/test/math_lib.js
+++ calc/test/math_lib.js
@@ -11,25 +11,16 @@ describe('#add()', function() {
     it('2 + -2 = 0', function() {
       assert.equal(math.add("2", "0"), 2);
     })
-    it('1.2 + 1.3 = 2.5', function() {
-      assert.equal(math.add("1.2", "1.3"), 2.5);
-    })
 })
 
 describe('#mul()', function() {
     it('4 * 7 = 28', function() {
-      assert.equal(math.mul(4,7), 28);
+      assert.equal(math.mul("4", "7"), 28);
     })
     it('9 * 0 = 0', function() {
-      assert.equal(math.mul(9,0), 0);
+      assert.equal(math.mul("9", "0"), 0);
     })
     it('0 * 8 = 0', function() {
-      assert.equal(math.mul(0,8), 0);
-    })
-    it('-5 * -5 = 25', function() {
-      assert.equal(math.mul(-5,-5), 25);
-    })
-    it('-5 * 3 = -15', function() {
-      assert.equal(math.mul(-5, 3), -15);
+      assert.equal(math.mul("0", "8"), 0);
     })
 })
